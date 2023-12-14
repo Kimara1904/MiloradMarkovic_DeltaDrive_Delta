652bcb77e4e4a03700c4da8d985d5ffaf7c6da79
Kimara1904 <mikipaok@gmail.com>
2023-12-14 16:02:23 +0100

    Improve GeoCalculator implementation

--- MiloradMarkovic_DeltaDrive_Delta/MiloradMarkovic_DeltaDrive_Delta/Helpers/GeoCalculator.cs
+++ MiloradMarkovic_DeltaDrive_Delta/MiloradMarkovic_DeltaDrive_Delta/Helpers/GeoCalculator.cs
@@ -1,4 +1,5 @@
-﻿using MiloradMarkovic_DeltaDrive_Delta.Helpers.Interfaces;
+﻿using GeoCoordinatePortable;
+using MiloradMarkovic_DeltaDrive_Delta.Helpers.Interfaces;
 
 namespace MiloradMarkovic_DeltaDrive_Delta.Helpers
 {
@@ -6,23 +7,10 @@ namespace MiloradMarkovic_DeltaDrive_Delta.Helpers
     {
         public async Task<double> CalculateDistance(double lat1, double lon1, double lat2, double lon2)
         {
-            double radLat1 = Math.PI * lat1 / 180;
-            double radLon1 = Math.PI * lon1 / 180;
-            double radLat2 = Math.PI * lat2 / 180;
-            double radLon2 = Math.PI * lon2 / 180;
+            var Coord1 = new GeoCoordinate(lat1, lon1);
+            var Coord2 = new GeoCoordinate(lat2, lon2);
 
-            double deltaLat = radLat2 - radLat1;
-            double deltaLon = radLon2 - radLon1;
-
-            double a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
-                   Math.Cos(radLat1) * Math.Cos(radLat2) *
-                   Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
-
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-            double distance = 6371 * c; // multiplying with radius of Earth
-
-            return distance;
+            return Coord1.GetDistanceTo(Coord2);
         }
     }
 }
